a78507d961c613a9a064340837def8d794c24a03
EunBae Gong <geb9211@gmail.com>
2024-01-18 22:04:07 +0900

    feat: globalStyle 설정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,8 +1,22 @@
-import { Global } from '@emotion/react';
+/** @jsxImportSource @emotion/react */
+import { Global, css } from '@emotion/react';
 import reset from './reset';
 
-const GlobalStyle = () => {
-  return <Global styles={reset} />;
+type Props = {
+  children: React.ReactElement;
+};
+
+const GlobalStyle = ({ children }: Props) => {
+  return (
+    <div
+      css={css`
+        width: 414px;
+        margin: 0 auto;
+      `}>
+      <Global styles={reset} />
+      {children}
+    </div>
+  );
 };
 
 export default GlobalStyle;
