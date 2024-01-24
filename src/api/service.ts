c3c1b2f3e3337bfdc2615a565702cc27fd4a2295
EunBae Gong <geb9211@gmail.com>
2024-01-25 00:07:14 +0900

    feat: APIClient 추상화

--- src/api/service.ts
+++ src/api/service.ts
@@ -1,27 +1,10 @@
+import APIClient from './client';
 import * as Type from './types';
 
-export default class APIService {
-  private static async APIclient<ResponseType>({
-    accessToken,
-    url,
-  }: {
-    accessToken: string;
-    url: string;
-  }): Promise<ResponseType> {
-    try {
-      const response = await fetch(`/core/${url}`, {
-        headers: { Authorization: accessToken },
-      });
-      return await response.json();
-    } catch (error) {
-      throw Error();
-    }
-  }
-
-  static async getLibraryHistories(accessToken: string) {
+export default class APIService extends APIClient {
+  static async getLibraryHistories() {
     return await this.APIclient<Type.GetLibraryHistoriesResponse>({
-      accessToken,
-      url: 'library-status',
+      url: 'library-status/recap',
     });
   }
 }
