671bee53744f9543fbd5b0f61495c20d4132d612
EunBae Gong <geb9211@gmail.com>
2024-01-26 06:26:08 +0900

    feat: defaultUser 선언

---
+++ src/constants/user.ts
@@ -0,0 +1,16 @@
+import { UserInfoType } from '@uoslife/webview';
+
+export const defaultUser = {
+  id: 0,
+  name: '시대생',
+  nickname: '시대인',
+  birthday: '',
+  phone: '000-0000-0000',
+  avatarUrl: '',
+  isVerified: false,
+  degree: '',
+  enrollmentStatus: '',
+  studentId: '0000000000',
+  departmentName: '경영학부',
+  collegeName: '경영대학',
+} satisfies UserInfoType;
