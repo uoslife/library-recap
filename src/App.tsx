711b7f77e4ce60c488510854269e69d485134788
EunBae Gong <geb9211@gmail.com>
2024-01-26 06:27:41 +0900

    feat: 앱 초기 로딩시 user, recapInfo를 받도록 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import GlobalStyle from './styles/GlobalStyle';
 import MainContainer from './components/containers/MainContainer/MainContainer';
+import useInitApp from './hooks/useInitApp';
 
 const App = () => {
+  useInitApp();
+
   return (
     <GlobalStyle>
       <MainContainer />
---
+++ src/hooks/useInitApp.ts
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import uoslifeBridge from '../bridge';
+import useUserStore from '../store/user';
+import APIService from '../api/service';
+import useRecapInfoStore from '../store/recapInfo';
+
+const useInitApp = () => {
+  const { setUser, setDefaultUser } = useUserStore();
+  const { setRecapInfo } = useRecapInfoStore();
+
+  const handleUserInfo = async () => {
+    try {
+      const user = await uoslifeBridge.getUserInfo();
+      setUser(user);
+    } catch (e) {
+      setDefaultUser();
+    }
+  };
+  const handleRecapInfo = async () => {
+    try {
+      const recapInfo = await APIService.getLibraryHistories();
+      setRecapInfo(recapInfo);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      handleUserInfo();
+      handleRecapInfo();
+    })();
+  }, []);
+};
+
+export default useInitApp;
