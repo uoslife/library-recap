867ec7ae42a171c5173526a0e5f056824a7f5a2b
EunBae Gong <geb9211@gmail.com>
2024-01-26 06:29:11 +0900

    feat: font-face 선언

---
+++ src/styles/font.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+
+export const fontFace = css`
+  @font-face {
+    font-family: 'Pretendard-Bold';
+    src: url('/fonts/Pretendard-Bold.otf');
+  }
+  @font-face {
+    font-family: 'Pretendard-SemiBold';
+    src: url('/fonts/Pretendard-SemiBold.otf');
+  }
+  @font-face {
+    font-family: 'Pretendard-Medium';
+    src: url('/fonts/Pretendard-Medium.otf');
+  }
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('/fonts/Pretendard-Regular.otf');
+  }
+  @font-face {
+    font-family: 'Pretendard-Light';
+    src: url('/fonts/Pretendard-Light.otf');
+  }
+`;
