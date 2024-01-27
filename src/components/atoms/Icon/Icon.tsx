85451c2bfcc7e1f234d10fdff002063ebe54d048
EunBae Gong <geb9211@gmail.com>
2024-01-27 23:48:04 +0900

    feat: Icon에 clear icon 추가

--- src/components/atoms/Icon/Icon.tsx
+++ src/components/atoms/Icon/Icon.tsx
@@ -1,7 +1,9 @@
-type Props = { name: 'chair' | 'clock' };
+type Props = {
+  name: 'chair' | 'clock' | 'clear';
+} & React.HTMLAttributes<HTMLImageElement>;
 
-const Icon = ({ name }: Props) => {
-  return <img src={`/images/icons/${name}.svg`} />;
+const Icon = ({ name, ...props }: Props) => {
+  return <img src={`/images/icons/${name}.svg`} {...props} />;
 };
 
 export default Icon;
