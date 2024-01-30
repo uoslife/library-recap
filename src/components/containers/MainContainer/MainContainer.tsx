a488b1e099ba33df0702ec66f05ade9019a9d84d
EunBae Gong <geb9211@gmail.com>
2024-01-31 04:49:41 +0900

    feat: 메인 버튼 눌렀을 때 API를 불러오도록 수정

--- src/components/containers/MainContainer/MainContainer.tsx
+++ src/components/containers/MainContainer/MainContainer.tsx
@@ -2,13 +2,16 @@ import { useCallback } from 'react';
 import MainPage from '../../pages/MainPage';
 import TimelineView from '../../templates/TimelineView';
 import useIsMainStore from '../../../store/isMain';
+import APIService from '../../../api';
 
 const MainContainer = () => {
   const { isMain, setTimelinePage } = useIsMainStore();
 
-  const setTimelineView = useCallback(() => {
+  const setTimelineView = useCallback(async () => {
     setTimelinePage();
     // eslint-disable-next-line react-hooks/exhaustive-deps
+    const recapInfo = await APIService.getLibraryHistories();
+    console.log(recapInfo);
   }, []);
 
   return isMain ? (
