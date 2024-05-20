39dfcb6ae4ffd1faa070ef5771a37383ea09afb8
EunBae Gong <geb9211@gmail.com>
2024-05-20 14:00:19 +0900

    feat: 변경된 userInfoType 대응

--- src/components/pages/LastTimeline/LastTimeline.tsx
+++ src/components/pages/LastTimeline/LastTimeline.tsx
@@ -44,95 +44,11 @@ const LastTimeline = () => {
           />
         </div>
         <Txt
-          label={'2024년의 여정도 시대생과 함께해요!'}
+          label={'시대생 도서관 페이지에서 2024년 여정도 확인할 수 있어요!'}
           color={'grey190'}
           typograph={'titleSmall'}
         />
       </div>
-      <ul
-        css={css`
-          width: 90%;
-          display: flex;
-          flex-direction: column;
-          gap: 16px;
-        `}>
-        <S.List>
-          <ListCountCircle count={1} />
-          <Txt
-            label={'마음에 드는 이벤트 페이지를 캡처하기'}
-            color={'grey190'}
-            typograph={'bodyLarge'}
-          />
-        </S.List>
-        <S.List>
-          <ListCountCircle count={2} />
-          <div
-            css={css`
-              display: flex;
-              flex-direction: column;
-            `}>
-            <Txt
-              label={'시대생 인스타그램 팔로우 후 해시태그'}
-              color={'grey190'}
-              typograph={'bodyLarge'}
-            />
-            <Txt
-              label={'(@uoslife_official)와 함께 스토리에 공유하기!'}
-              color={'grey190'}
-              typograph={'bodyLarge'}
-            />
-          </div>
-        </S.List>
-        <S.List>
-          <ListCountCircle count={3} />
-          <div
-            css={css`
-              display: flex;
-              flex-direction: column;
-            `}>
-            <Txt
-              label={'시대생 도서관 페이지에서 2024년 여정도'}
-              color={'grey190'}
-              typograph={'bodyLarge'}
-            />
-            <Txt
-              label={'확인할 수 있어요!'}
-              color={'grey190'}
-              typograph={'bodyLarge'}
-            />
-          </div>
-        </S.List>
-        <S.List>
-          <ListCountCircle count={4} />
-
-          <div
-            css={css`
-              display: flex;
-              flex-direction: column;
-            `}>
-            <div
-              css={css`
-                display: flex;
-              `}>
-              <Txt
-                label={'홈 화면 배너'}
-                color={'primaryBrand'}
-                typograph={'bodyLarge'}
-              />
-              <Txt
-                label={'를 통해 언제든 다시 2023년'}
-                color={'grey190'}
-                typograph={'bodyLarge'}
-              />
-            </div>
-            <Txt
-              label={'도서관 여정을 확인할 수 있어요~'}
-              color={'grey190'}
-              typograph={'bodyLarge'}
-            />
-          </div>
-        </S.List>
-      </ul>
     </S.Container>
   );
 };
--- src/constants/user.ts
+++ src/constants/user.ts
@@ -2,15 +2,17 @@ import { UserInfoType } from '@uoslife/webview';
 
 export const defaultUser = {
   id: 0,
-  name: '시대생',
   nickname: '시대인',
-  birthday: '',
-  phone: '000-0000-0000',
-  avatarUrl: '',
+  name: '시대생',
+  email: '',
+  identity: {
+    id: '0',
+    type: '학사',
+    status: '학부생',
+    idNumber: '0000000000',
+    university: '경영학부',
+    department: '',
+    major: '경영학부',
+  },
   isVerified: true,
-  degree: '',
-  enrollmentStatus: '',
-  studentId: '0000000000',
-  departmentName: '경영학부',
-  collegeName: '경영대학',
-} satisfies UserInfoType;
+} as UserInfoType;
